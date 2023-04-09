29c1140a1a2e84dc8d185c8aec89fa510dcc5197
yabindra-bhujel <s202137053@shikoku-u.ac.jp>
2023-04-09 12:28:38 +0900

    add allow host

--- mysite/settings.py
+++ mysite/settings.py
@@ -1,5 +1,7 @@
 from datetime import timedelta
+import os
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -171,6 +173,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
+django_heroku.settings(locals())
+
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
